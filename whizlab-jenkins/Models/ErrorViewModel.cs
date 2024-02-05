5f43c947c4a7f13ef887beb6a4ddd6ee2a3a104a
Javier Armando <javier.avalos.yaya@gmail.com>
2024-02-05 23:11:59 +0000

    All project

---
+++ whizlab-jenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace whizlab_jenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
